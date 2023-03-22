d6629ebfc9f06c148d200a3ca53968c1f415501d
Alana Wilcoxson <alana.wilcoxson@utexas.edu>
2023-03-21 21:48:18 -0500

    Added the Header with spinning logo

--- client/src/AwesomeApp.js
+++ client/src/AwesomeApp.js
@@ -2,10 +2,24 @@ import React from "react";
 import logo from './logo.svg';
 import './App.css';
 
+class Header extends React.Component {
+        render(){
+        return (
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <div>
+                    <h1>This is AWESOME App</h1>
+                    <p>We manage your projects so you don't have to...because we're AWESOME.</p>
+                </div>
+            </header>
+        );
+    }
+}
+
 function AwesomeApp(){
     return (
         <div className={"App"}>
-            <h1>This is Awesome App.</h1>
+            <Header />
         </div>
     )
 }
